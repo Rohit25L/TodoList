0d498c5e1d07cc682efdcfd277adae45e8eecc38
RohitE25 <kumarrohit.2512003@gmail.com>
2025-07-26 23:44:07 +0530

    nothing

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
-let queue =[];
+// let queue =[];
 
-queue.push(10)
-queue.push(11)
-queue.push(12)
-console.log(queue)
-queue.shift()
+// queue.push(10)
+// queue.push(11)
+// queue.push(12)
+// console.log(queue)
+// queue.shift()
 
-console.log(queue)
+// console.log(queue)
+let a =[2,3]
+let map =new Map()
 
+map.set(a,5)
 
-
-console.log(queue[0])
+if(map.has([2,3])){
+console.log(map.get(a))
+}
